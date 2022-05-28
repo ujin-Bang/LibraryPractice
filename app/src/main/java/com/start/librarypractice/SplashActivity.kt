c6091ab76c9071a398c3912419d73f733c06e682
ujin-Bang <qkddnwls14@gmail.com>
2022-05-28 12:35:24 +0900

    로딩 화면 추가

---
+++ app/src/main/java/com/start/librarypractice/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.start.librarypractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+    fun setValues(){
+
+    }
+}
