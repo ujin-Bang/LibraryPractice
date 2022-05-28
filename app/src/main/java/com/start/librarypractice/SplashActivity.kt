01ec63515fa56a0f143b0d2792c343c5afc5909a
ujin-Bang <qkddnwls14@gmail.com>
2022-05-28 12:47:12 +0900

    2.5초후에 메인으로 이동하도록

--- app/src/main/java/com/start/librarypractice/SplashActivity.kt
+++ app/src/main/java/com/start/librarypractice/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.start.librarypractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +14,21 @@ class SplashActivity : AppCompatActivity() {
         setValues()
     }
 
-    fun setupEvents(){
+    fun setupEvents() {
 
     }
-    fun setValues(){
 
+    fun setValues() {
+
+//      2.5초 후에 메인화면으로 이동 => SplashActivity는 종료
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+
+        }, 2500)
     }
 }
